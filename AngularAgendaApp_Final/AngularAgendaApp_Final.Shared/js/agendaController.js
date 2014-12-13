95f86b7aa01d4ca70c543b0b414e74237c78bd50
codingyourlife <haniballsadresse@gmx.at>
2014-12-13 01:44:25 +0100

    initial commit

---
+++ AngularAgendaApp_Final/AngularAgendaApp_Final.Shared/js/agendaController.js
@@ -0,0 +1,79 @@
+﻿(function () {
+
+    // AngularJS Stuff
+    var app = angular.module('app', ['ngRoute']);
+
+    app.controller('AgendaController', function ($scope, $http) {
+        $scope.Talks = [];
+
+        $scope.talkisnow = function (talk) {
+            var now = new Date();
+            //var difference_Start_ms = now - new Date(2014, 11, 5);//talk.StartTime;
+            //var difference_End_ms = /*talk.EndTime*/ new Date(2014, 11, 7) - now;
+            var difference_Start_ms = now - new Date(talk.StartTime);
+            var difference_End_ms = new Date(talk.EndTime) - now;
+            return (difference_Start_ms > 0) && (difference_End_ms > 0);
+        }
+
+        $http.get('http://mdaw.azurewebsites.net/api/Agenda').
+            success(function (data) {
+                $scope.Talks = data.Talks;
+            }).error(function () {
+                debugger;
+            });
+    });
+
+    app.controller('SpeakersController', function ($scope, $http) {
+        $scope.Speakers = [];
+
+        $http.get('http://mdaw.azurewebsites.net/api/Speakers').
+            success(function (data) {
+                $scope.Speakers = data.Speakers;
+            }).error(function () {
+                debugger;
+            });
+    });
+
+    app.controller('SponsorsController', function ($scope, $http) {
+        $scope.Sponsors = [];
+
+        $http.get('http://mdaw.azurewebsites.net/api/Sponsors').
+            success(function (data) {
+                $scope.Sponsors = data.Sponsors;
+            }).error(function () {
+                debugger;
+            });
+    });
+
+    app.controller('AgendaDetails', ['$scope', '$routeParams',
+      function ($scope, $routeParams) {
+          $scope.talkId = $routeParams.talkId;
+      }]);
+
+    app.directive('agendaHub', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'directive/agenda-hub.html',
+            compile: function (element) {
+                var ui = WinJS.UI;
+                ui.processAll(element[0]);
+            }
+        };
+    });
+
+    app.config(function ($routeProvider) {
+        $routeProvider.
+          when('/agenda', {
+              template: '<agenda-hub></agenda-hub>'
+          })
+          .when('/talks/:talkId', {
+              templateUrl: 'directive/agenda-details.html',
+              controller: 'AgendaDetails'
+          })
+          .otherwise({
+              redirectTo: '/agenda'
+          });
+    });
+})();
+
+
---
+++ UniversalAppInBrowser/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace UniversalAppInBrowser
+{
+    using System.Diagnostics;
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sharedDir = "\"file://" + Directory.GetCurrentDirectory() + "..\\..\\..\\..\\AngularAgendaApp_Final\\AngularAgendaApp_Final.Shared\\default.html\"";
+
+            var proc1 = new ProcessStartInfo();
+            string anyCommand = "chrome.exe " + sharedDir + " -disable-web-security";
+            proc1.UseShellExecute = true;
+
+            proc1.WorkingDirectory = @"C:\\Program Files (x86)\\Google\\Chrome\\Application\\";
+
+            proc1.FileName = @"C:\Windows\System32\cmd.exe";
+            //proc1.Verb = "runas";
+            proc1.Arguments = "/c " + anyCommand;
+            proc1.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(proc1);
+        }
+    }
+}
